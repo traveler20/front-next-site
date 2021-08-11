f0f2ee51ff6b457e91237b89bd876e641e4f9398
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2021-08-11 15:53:33 +0900

    ✨New Feature v2

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -38,6 +38,10 @@ export async function getStaticProps() {
 		return data;
 	})(require.context("../data", true, /\.md$/));
 
+	const orderedBlogs = blogs.sort((a, b) => {
+		return b.frontmatter.id - a.frontmatter.id;
+	});
+
 	return {
 		props: {
 			blogs: blogs,
