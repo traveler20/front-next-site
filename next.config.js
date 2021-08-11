7b6eb6c7bebda3d59a4fcb0a6cb86f44195a1eeb
tabito.otsubo <tabibito.traveler.sea.1039@gmail.com>
2021-08-12 08:44:45 +0900

    ✨Update Feature v5

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-	reactStrictMode: true,
 	webpack: function (config) {
 		config.module.rules.push({
 			test: /\.md$/,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
 import Layout from "../components/layout";
+import Seo from "../components/seo";
 import * as style from "../styles/index.module.scss";
 
 const Index = () => {
 	return (
 		<Layout>
+			<Seo
+				title="Abe Hiroki"
+				description="Abe Hirokiのポートフォリオサイトです"
+			/>
 			<div className={style.hero}>
 				<Image
 					src="/images/index-hero.jpg"
